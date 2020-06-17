33f2fa2b494069ece2751ad722f8098850bba389
AnnShul <lp-08>
2020-06-17 18:36:36 +0300

    'classes'

--- src/App.js
+++ src/App.js
@@ -1,66 +1,36 @@
 
-//________Прототипы_______
+//______Classes______
 
-//const animal выводит функцию say для cat и dog
-const animal = {
-    say: function () {
-        console.log(this.name, 'goes', this.voice);
-    }
-};
-const dog = {
-    name: 'dog',
-    voice: 'woof',
-    };
-//Object.setPrototypeOf делигирует say to const animal (animal это прототип dog)
-Object.setPrototypeOf(dog, animal);
-// Object.setPrototypeOf - лучше не использовтаь данный метод
-dog.say();
-
-//better next
-
-
-function createAnimal(name, voice) {
-    const result = Object.create(animal);
-    result.name = name;
-    result.voice = voice;
-    return result;
-}
-
-const dog = createAnimal('dog', 'woof');
-const cat = createAnimal('cat', 'meow');
-
-dog.say();
-cat.say();
-
-//or
-
-const dog = Object.create(animal);
-dog.name = 'dog';
-dog.voice = 'woof';
-dog.say();
-
-
-// const cat = {
-//     name: 'cat',
-//     voice: 'meow',
-//     };
-// Object.setPrototypeOf(cat, animal);
+// function Animal(name, voice) {
+//     this.name = name;
+//     this.voice = voice;
+// }
+// Animal.prototype.say = function() {
+//     console.log(this.name, 'goes', this.voice);
+// };
+// const dog = new Animal('dog', 'woof');
+// const cat = new Animal('cat', 'meow');
+//
 // dog.say();
 // cat.say();
 
-function Animal(name, voice) {
-    this.name = name;
-    this.voice = voice;
+class Animal {
+    constructor(name, voice) {
+        this.name = name;
+        this.voice= voice;
+   }
+   say() {
+        console.log(this.name, 'goes', this.voice);
+   }
+}
+class Bird extends Animal {
+constructor(name, voice, canFly) {
+    //конструктор ключегового класса
+    super(name, voice);
+    this.canFly = canFly;
     }
-Animal.prototype.say = function() {
-    console.log(this.name, 'goes', this.voice);
-};
-const dog = new Animal('dog', 'woof');
-const cat = new Animal('cat', 'meow');
-
-dog.say();
-cat.say();
-
-//1. Object.setPrototypeOf
-//2. Object.create
-//3. using new
+    say() {
+    console.log('Birds dont like to talk')
+    }
+}
+const duck = new Bird('Duck', 'quack');
