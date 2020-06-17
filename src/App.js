9e1c8849bb23ec9a7561bfddfddd2470774c6ddb
AnnShul <lp-08>
2020-06-17 15:28:31 +0300

    'first'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 
 
 //Объеткы в ES-2015+
-//__________Template Literals Шаблонные строки_____________
-const user = 'Bob';
-const num = 17;
-const txt2 = `Hello ${user} you ${num} letters in your inbox`;
-console.log(txt2);
-//Hello Bob you 17 letters in your inbox
-
-const items = ['tea', 'coffee'];
-const templateHtml = `
-<ui>
-<li>${items[0]}</li>
-<li>${items[1]}</li>
-</ui>`
-console.log(templateHtml)
-
+const defaults = {
+    host:
+}
