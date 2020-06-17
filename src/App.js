5f9062a32a780458d28daec196ad8151450bb0e7
AnnShul <lp-08>
2020-06-17 17:42:45 +0300

    prototype

--- src/App.js
+++ src/App.js
@@ -1,16 +1,66 @@
-//_______Объеткы в ES-2015+________
-//опции по умолчанию
-const defaults = {
-    host: 'localhost',
-    dbName: 'blog',
-    user: 'admin' };
-//опции от пользовтеля
-const opts = {
-    user: 'John',
-    password: 'utopia'
+
+//________Прототипы_______
+
+//const animal выводит функцию say для cat и dog
+const animal = {
+    say: function () {
+        console.log(this.name, 'goes', this.voice);
+    }
 };
-//Object.assign method
-const res = Object.assign({}, defaults, opts);
-console.log(res);
+const dog = {
+    name: 'dog',
+    voice: 'woof',
+    };
+//Object.setPrototypeOf делигирует say to const animal (animal это прототип dog)
+Object.setPrototypeOf(dog, animal);
+// Object.setPrototypeOf - лучше не использовтаь данный метод
+dog.say();
+
+//better next
 
 
+function createAnimal(name, voice) {
+    const result = Object.create(animal);
+    result.name = name;
+    result.voice = voice;
+    return result;
+}
+
+const dog = createAnimal('dog', 'woof');
+const cat = createAnimal('cat', 'meow');
+
+dog.say();
+cat.say();
+
+//or
+
+const dog = Object.create(animal);
+dog.name = 'dog';
+dog.voice = 'woof';
+dog.say();
+
+
+// const cat = {
+//     name: 'cat',
+//     voice: 'meow',
+//     };
+// Object.setPrototypeOf(cat, animal);
+// dog.say();
+// cat.say();
+
+function Animal(name, voice) {
+    this.name = name;
+    this.voice = voice;
+    }
+Animal.prototype.say = function() {
+    console.log(this.name, 'goes', this.voice);
+};
+const dog = new Animal('dog', 'woof');
+const cat = new Animal('cat', 'meow');
+
+dog.say();
+cat.say();
+
+//1. Object.setPrototypeOf
+//2. Object.create
+//3. using new
