78e2fac53335e072575f10bf2bac9a833dee8d11
AnnShul <lp-08>
2020-06-18 17:49:31 +0300

    'React_компоненты'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
+//важно - подключиться к библиотеке, важно соблюдать прописные и строчные буквы
 import React from 'react';
+//react очень легковесный и поэтому быстро работает
 import ReactDom from 'react-dom';
 
-const el = <h1>Hello World</h1>
-//если писать на реакте
-// const el = React.createElement(
-//     'h1', null, 'Hello World!!');
-console.log(el);
+//функции которые возвращают реакт элемен должны начинаться с большой буквы
+const AppHeader = () => {
+    return <h1>My Todo List</h1>;
+};
 
-ReactDom.render(el,
+//вынести в отдельный компонент список дел
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    );
+};
+
+const SearchPanel = () => {
+    return <input placeholder="search"/>;
+};
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader/>
+            <TodoList/>
+            <SearchPanel/>
+        </div>);
+    }
+
+
+//превращает React элементы в обычные браузерные DOM элементы и рендерит их на странице
+ReactDom.render(<App/>,
     document.getElementById('root'));
