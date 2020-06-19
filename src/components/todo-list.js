f2b3033f68e0d453484933f64fa410330158deed
AnnShul <lp-08>
2020-06-19 17:00:46 +0300

    'Arrows_as_props'

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,14 +1,24 @@
 import React from "react";
 import TodoListItem from "./todo-list-item"
-const TodoList = () => {
-    const items = ['Drink Coffee', 'Build Awesome App'];
+
+const TodoList = ({todos}) => {
+    // const items = ['Drink Coffee', 'Build Awesome App', 'Have a lunch'];
+    const elements = todos.map((item) => {
+        //для каждого элемента массива todos создаем соответ. JSX элемент return ниже
+        return (
+            <li>
+                {/*можно передать все свойства объекта item в компонент используя Object Spread оператор*/}
+                <TodoListItem {...item}
+                    // label={item.label}
+                    // important={item.important} - все это компактно заменили на {...item}
+                />
+            </li>
+        );
+    });
+    //для вывода на экран
     return (
         <ul>
-            <li><TodoListItem label="Drink Coffee"/></li>
-            <li><TodoListItem
-                label="Build React App"
-                important/>
-            </li>
+            { elements }
         </ul>
     );
 };
--- src/index.js
+++ src/index.js
@@ -6,10 +6,16 @@ import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
 const App = () => {
+    //в JSX можно вставлять массивы элементов (массив можно передать как свойство
+    const todoData = [
+        {label:'Drink Coffee', important: false},
+        {label:'Make Awesome App', important: true},
+        {label:'Have a lunch', important: true},
+    ];
     return (
         <div>
             <AppHeader/>
-            <TodoList/>
+            <TodoList todos={todoData}/>
             <SearchPanel/>
         </div>);
     }
