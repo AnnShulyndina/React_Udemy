d8068c44ab0c72bd126be55eac89c52d01e05c63
AnnShul <lp-08>
2020-06-22 15:21:57 +0300

    setState

---
+++ src/components/app/index.js
@@ -0,0 +1,4 @@
+import App from './app';
+
+export default App;
+//если в папке есть файл index.js то он импортируется по умолчанию
--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
-
-const App = () => {
-    //в JSX можно вставлять массивы элементов (массив можно передать как свойство
-    const todoData = [
-        {label:'Drink Coffee', important: false, id: 1},
-        {label:'Make Awesome App', important: true, id: 2},
-        {label:'Have a lunch', important: true, id: 3},
-    ];
-    return (
-        <div>
-            <AppHeader/>
-            <TodoList todos={todoData}/>
-            <SearchPanel/>
-        </div>);
-    }
+import App from './components/app';
 
 ReactDom.render(<App/>,
     document.getElementById('root'));
