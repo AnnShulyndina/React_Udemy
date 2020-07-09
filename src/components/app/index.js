a120969b30e72f344cf55026ffac7a0a36c0f561
AnnShul <lp-08>
2020-07-09 11:50:37 +0300

    All

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,4 +1,3 @@
 import App from './app';
 
 export default App;
-//если в папке есть файл index.js то он импортируется по умолчанию
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,70 +3,44 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
-    state = {
-        done: false,
-        important: false
-    };
 
+  render() {
 
-    // constructor(props) {
-    //     super(props);
-    //     this.onLabelClick= ()=> {
-    //         console.log (`Done: ${this.props.label}`)
-    //     };
-    // }
+    const { label, onDeleted,
+            onToggleImportant,
+            onToggleDone,
+            important, done } = this.props;
 
-    onLabelClick = () => {
-        this.setState(({done}) => {
-            return {
-                done: !done
-            };
-        });
-    };
-//setState () нужно передавать только изменения в state
-    onMarkImportant = () => {
-        this.setState(({important}) => {
-            return {
-                important: !important
-            };
-        });
-    };
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
 
-    render() {
-        const { label } = this.props;
-        const { done, important } = this.state;
+    if (important) {
+      classNames += ' important';
+    }
 
-        let classNames = 'todo-list-item';
-        if (done) {
-            classNames += ' done';
-        }
-
-        if (important) {
-            classNames += ' important';
-        }
-
-        return (
-            <span className={classNames}>
+    return (
+      <span className={classNames}>
         <span
-            className="todo-list-item-label"
-            onClick={ this.onLabelClick }>
+          className="todo-list-item-label"
+          onClick={onToggleDone}>
           {label}
         </span>
 
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
-          <i className="fa fa-trash-o" />
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={onToggleImportant}>
+          <i className="fa fa-exclamation" />
         </button>
 
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right"
-                onClick={this.onMarkImportant}>
-          <i className="fa fa-exclamation" />
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleted}>
+          <i className="fa fa-trash-o" />
         </button>
-            </span>
-        )
-            ;
-    };
+      </span>
+    );
+  };
 }
-
 
